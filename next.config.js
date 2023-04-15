6f9987af7462400b72f422f3b6bdd5d1834f0deb
Abimael M <abimaelmercado05@gmail.com>
2023-04-15 14:39:02 -0400

    extra

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
+const withImages = require("next-images");
+
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['speedy-tune-ups.vercel.app'],
-    loader: 'default',
-  },
-};
-
-module.exports = nextConfig;
+}
+module.exports = withImages(nextConfig);
