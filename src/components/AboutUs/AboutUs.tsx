2a18ef197b621bec6f61991b70eac45492158240
Abimael M <abimaelmercado05@gmail.com>
2023-04-16 01:05:49 -0400

    packages update, More Images

--- src/components/AboutUs/AboutUs.tsx
+++ src/components/AboutUs/AboutUs.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export const AboutUs = () => {
   return (
@@ -10,18 +11,18 @@ export const AboutUs = () => {
             Speedy Tune-Up&apos;s Fleet Services is family owned and operated,
             proudly servicing Miami-Dade, Broward and Palm Beach since 2007.
           </p>
-          <div className="w-2/3 md:w-80 mx-auto">
+          <div className="md:w-96 mx-auto">
             <Image
               width={250}
               height={100}
               priority={true}
-              src="/images/owners.png"
-              alt="Who we are"
-              className="w-full h-auto rounded-md shadow-md mt-6"
+              src="/images/frontShopView.jpeg"
+              alt="Front View of Shop"
+              className="w-fit mx-auto rounded-md shadow-md mt-6"
             />
           </div>
         </div>
-        <div className="w-2/3 md:w-80">
+        <div className="md:w-80">
           <Image
             width={250}
             height={100}
@@ -61,16 +62,33 @@ export const AboutUs = () => {
           him as part of the Speedy Team.
         </p>
       </section>
-      <section className="mt-16">
-        <h2 className="text-xl font-semibold mb-2">Services we offer</h2>
-        <ul className="list-disc pl-6">
-          <li>24-hour on-location mobile fleet service **</li>
-          <li>Complete Auto Care Center</li>
-          <li>Muffler Repair</li>
-          <li>Brakes Service</li>
-          <li>Engine Overhaul</li>
-          <li>Other services to ensure the life of your car</li>
-        </ul>
+      <section className="mt-14 w-full flex md:flex-row flex-col">
+        <div className="flex flex-col mb-4">
+          <h2 className="text-3xl font-semibold mb-4">Services we offer</h2>
+          <ul className="list-disc pl-6 text-lg">
+            <li>24-hour on-location mobile fleet service **</li>
+            <li>Complete Auto Care Center</li>
+            <li>Muffler Repair</li>
+            <li>Brakes Service</li>
+            <li>Oil Service</li>
+          </ul>
+          <Link
+            href="/services"
+            aria-label="click here button for more services"
+            className="w-fit p-4 m-4 rounded-lg duration-300 font-medium bg-lime-500 text-black hover:bg-lime-600 hover:text-white"
+          >
+            More services here
+          </Link>
+        </div>
+        <div className="w-fit md:w-2/6 mx-auto">
+          <Image
+            width={250}
+            height={100}
+            src="/images/owners.png"
+            alt="Owners"
+            className="w-full h-auto rounded-md shadow-md max-w-96"
+          />
+        </div>
       </section>
     </main>
   );
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   const currentYear = new Date().getFullYear();
   return (
-    <footer className="bg-gray-200 py-4 mt-[2rem]">
+    <footer className="bg-gray-200 py-6 mt-[2rem]">
       <div className="container mx-auto text-center">
         <p className="text-md text-black">
           Copyright &copy; 2016 - {currentYear} Speedy Tune Up&apos;s Fleet
